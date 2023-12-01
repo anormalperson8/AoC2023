fc5230269269f69497d0e6eb7de73d1a258502c7
anormalperson8 <133204516+anormalperson8@users.noreply.github.com>
2023-12-01 22:46:47 +0800

    Alternate way of solving day 1 after learning String.startsWith() exists

---
+++ src/main/java/AoC2023/Day1/AlternateNumberStringParser.java
@@ -0,0 +1,46 @@
+package AoC2023.Day1;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.IntStream;
+
+public class AlternateNumberStringParser {
+    private final List<String> lines = new ArrayList<>();
+
+    // Get all the things for matching
+    private final List<String> patternStrings;
+
+    public AlternateNumberStringParser(List<String> lines, boolean b) {
+        patternStrings = b ? List.of(
+                "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+                "0", "1", "2", "3", "4", "5", "6", "7", "8", "9") :
+                List.of("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+        this.lines.addAll(lines);
+    }
+
+    public Integer getSum() {
+        return this.lines.stream().map(i -> {
+            List<Integer> l = new ArrayList<>();
+            parseLine(i, l);
+            return l.get(0) * 10 + l.get(l.size() - 1);
+        }).mapToInt(Integer::intValue).sum();
+    }
+
+    private void parseLine(String line, List<Integer> l) {
+        if (line.isEmpty())
+            return;
+
+        patternStrings.forEach(i -> {
+            if (line.startsWith(i))
+                l.add(patternStrings.indexOf(i) % 10);
+        });
+
+        parseLine(line.substring(1), l);
+    }
+
+
+}
