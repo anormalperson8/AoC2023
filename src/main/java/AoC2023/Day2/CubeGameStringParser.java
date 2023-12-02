8c1ffa47daf0e329c9f2f4d2179a96b79500beb9
anormalperson8 <133204516+anormalperson8@users.noreply.github.com>
2023-12-02 16:17:30 +0800

    Reformatted indents

--- src/main/java/AoC2023/Day2/CubeGameStringParser.java
+++ src/main/java/AoC2023/Day2/CubeGameStringParser.java
@@ -41,21 +41,21 @@ public class CubeGameStringParser {
 
     public int parseLines() {
         return this.lines.stream().map(line -> Stream.of(line.split(": ")[1].split("; "))
-            .map(i -> List.of(i.split(", "))).map(i -> i.stream().map(j -> {
-                switch (j.split(" ")[1]) {
-                    case "red" -> {
-                        return Integer.parseInt(j.split(" ")[0]) <= CubeGameStringParser.RED;
+                .map(i -> List.of(i.split(", "))).map(i -> i.stream().map(j -> {
+                    switch (j.split(" ")[1]) {
+                        case "red" -> {
+                            return Integer.parseInt(j.split(" ")[0]) <= CubeGameStringParser.RED;
+                        }
+                        case "green" -> {
+                            return Integer.parseInt(j.split(" ")[0]) <= CubeGameStringParser.GREEN;
+                        }
+                        case "blue" -> {
+                            return Integer.parseInt(j.split(" ")[0]) <= CubeGameStringParser.BLUE;
+                        }
+                        default -> throw new IllegalArgumentException("Invalid colour.");
                     }
-                    case "green" -> {
-                        return Integer.parseInt(j.split(" ")[0]) <= CubeGameStringParser.GREEN;
-                    }
-                    case "blue" -> {
-                        return Integer.parseInt(j.split(" ")[0]) <= CubeGameStringParser.BLUE;
-                    }
-                    default -> throw new IllegalArgumentException("Invalid colour.");
-                }
-            }).reduce(true, (k, l) -> k && l))
-            .reduce(true, (k, l) -> k && l) ?
-            Integer.parseInt(line.split(": ")[0].replaceAll("Game ", "")) : 0).mapToInt(Integer::intValue).sum();
+                }).reduce(true, (k, l) -> k && l))
+                .reduce(true, (k, l) -> k && l) ?
+                Integer.parseInt(line.split(": ")[0].replaceAll("Game ", "")) : 0).mapToInt(Integer::intValue).sum();
     }
 }
