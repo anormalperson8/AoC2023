85a6aad3135b6b8a6dc8eeca774aeaac584a4e93
anormalperson8 <133204516+anormalperson8@users.noreply.github.com>
2023-12-02 15:49:36 +0800

    Day 2, part 1

---
+++ src/main/java/AoC2023/Day2/CubeGameStringParser.java
@@ -0,0 +1,41 @@
+package AoC2023.Day2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class CubeGameStringParser {
+    private final List<String> lines;
+    private static final int RED = 12;
+    private static final int GREEN = 13;
+    private static final int BLUE = 14;
+
+
+    public CubeGameStringParser(List<String> lines) {
+        this.lines = new ArrayList<>(lines);
+    }
+
+    public int get() {
+        return this.lines.stream().map(this::parseLine).mapToInt(Integer::intValue).sum();
+    }
+
+    private int parseLine(String line) {
+        return Stream.of(line.split(": ")[1].split("; "))
+                .map(i -> List.of(i.split(", "))).map(i -> i.stream().map(j -> {
+                    switch (j.split(" ")[1]) {
+                        case "red" -> {
+                            return Integer.parseInt(j.split(" ")[0]) <= CubeGameStringParser.RED;
+                        }
+                        case "green" -> {
+                            return Integer.parseInt(j.split(" ")[0]) <= CubeGameStringParser.GREEN;
+                        }
+                        case "blue" -> {
+                            return Integer.parseInt(j.split(" ")[0]) <= CubeGameStringParser.BLUE;
+                        }
+                        default -> throw new IllegalArgumentException("Invalid colour.");
+                    }
+                }).reduce(true, (k, l) -> k && l))
+                .reduce(true, (k, l) -> k && l) ?
+                Integer.parseInt(line.split(": ")[0].replaceAll("Game ", "")) : 0;
+    }
+}
