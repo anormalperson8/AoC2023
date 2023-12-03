aa786fb7579140e8ec311422d43f53d3f948ebb1
anormalperson8 <133204516+anormalperson8@users.noreply.github.com>
2023-12-04 01:27:22 +0800

    Changed up FileInputTest. Don't ask why

--- src/test/java/aoc2023/FileInputTest.java
+++ src/test/java/aoc2023/FileInputTest.java
@@ -1,35 +1,26 @@
 package aoc2023;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FileInputTest {
-
-    private FileInput fileInput;
-    private int a = 1;
-
-    @BeforeEach
-    public void setUp() {
-        fileInput = new FileInput("input" + File.separator + "AoC2023_" + a++ + ".txt");
+    @Test
+    void testTest() {
+        FileInput fileInputTest = new FileInput( "input" + File.separator + "FileInputTest.txt");
+        List<String> l = new ArrayList<>();
+        IntStream.range(0, 16).forEach(i -> l.add("abc"));
+        assertArrayEquals(l.toArray(), fileInputTest.getLines().toArray());
+        assertArrayEquals(l.subList(0, 10).toArray(), fileInputTest.test().toArray());
     }
 
     @Test
-    void testTest() {
-        String testString = """
-                threehqv2
-                sxoneightoneckk9ldctxxnffqnzmjqvj
-                1hggcqcstgpmg26lzxtltcgg
-                nrhoneightkmrjkd7fivesixklvvfnmhsldrc
-                zhlzhrkljonephkgdzsnlglmxvprlh6n
-                594chhplnzsxktjmkfpninefiveczfnvsctbxcfzfzjh
-                seven2tjf
-                five712
-                tzheightzlzmsqlnxfqzrlbhbdpbnbdjns6
-                ztwo1eight95""";
-        assertEquals(testString, String.join("\n", fileInput.test()));
+    void testException() {
+        assertThrows(RuntimeException.class, () -> new FileInput("input" + File.separator + "AoC2023_26.txt"));
     }
 }
